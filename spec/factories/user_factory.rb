a66cd8ec118052a21da75ca724ffb6598919fc40
Tsvetelina Borisova <ts.borisova3@gmail.com>
2014-07-30 14:11:02 +0300

    Added admin flag to user

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     email "example2@example.com"
     password 'PAssword'
     password_confirmation 'PAssword'
+    
+    factory :admin_user do
+      admin true
+    end
   end
 end
