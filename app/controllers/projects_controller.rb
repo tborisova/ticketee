ab873685fa60719ddb142a67cbd0ffa750a23fa0
Tsvetelina Borisova <ts.borisova3@gmail.com>
2014-07-30 15:31:01 +0300

    Don't show projects that a user doesn't have permission to see

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    @projects = Project.for(current_user)
   end
 
   def new
@@ -51,11 +51,7 @@ class ProjectsController < ApplicationController
     end
     
     def set_project
-      @project = if current_user.admin?
-        Project.find(params[:id])
-      else
-        Project.viewable_by(current_user).find(params[:id])
-      end
+      @project = Project.for(current_user).find(params[:id])
 
       rescue ActiveRecord::RecordNotFound
         flash[:alert] = "The project you were looking for could not be found."
--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -10,8 +10,10 @@ feature "Viewing projects" do
   end
 
   scenario "Listing all projects" do
+    FactoryGirl.create(:project, name: "Hidden")
+    
     visit '/'
-
+    expect(page).to_not have_content("Hidden")
     click_link project.name
     
     expect(page.current_url).to eql(project_url(project))
