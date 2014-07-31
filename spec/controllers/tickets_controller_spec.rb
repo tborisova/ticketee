555eb11a8e494de78202810f05192c62e69e1fa0
Tsvetelina Borisova <ts.borisova3@gmail.com>
2014-07-31 14:41:32 +0300

    Restrict reading tickets to correct project scope

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, :type => :controller do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:project) { FactoryGirl.create(:project) }
+  let(:ticket) { FactoryGirl.create(:ticket, project: project, user: user) }
 
+  context 'standard users' do
+    it 'cannot access a ticket for a project' do
+      sign_in user
+
+      get :show, id: ticket, project_id: project
+
+      expect(response).to redirect_to root_path
+      expect(flash[:alert]).to eq 'The project you were looking for could not be found.'
+    end
+  end
 end
